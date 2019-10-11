83e584f82311152237baa0dcbb4c4d659f85223c
Djavid Gabibiyan <djavidoff@gmail.com>
2019-10-11 23:52:22 +0500

    Docker deploy script
    * Remove `travis-deploy.sh`
    
    * Add `deploy.sh` script to deploy on Docker Hub
    
    * Modify `.travis.yml` to run docker deploy script
    
    * Fix image name in deploy script
    
    * Update library version to `0.1.1`
    
    * Rename deploy script
    
    * Remove version `8` of Node from Travis config
    
    * Bring back version `8`, check for LTS version of Node
    
    * Update .travis.yml

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -1,5 +1,6 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
+# Show script in output, and error if anything fails
 set -xe
 
 # Update this whenever the latest Node.js LTS version changes (~ every year).
@@ -15,17 +16,30 @@ if [ "$TRAVIS_NODE_VERSION" != "$LATEST_LTS_VERSION" ]; then
 fi
 
 # Ensure the tag matches the one in package.json, otherwise abort.
-PACKAGE_TAG=v"$(jq -r .version package.json)"
-if [ "$PACKAGE_TAG" != "$TRAVIS_TAG" ]; then
+VERSION="$(jq -r .version package.json)"
+PACKAGE_TAG=v"$VERSION"
+
+if [[ "$PACKAGE_TAG" != "$TRAVIS_TAG" ]]; then
   echo "Travis tag (\"$TRAVIS_TAG\") is not equal to package.json tag (\"$PACKAGE_TAG\"). Please push a correct tag and try again."
   exit 1
 fi
 
+IMAGE_NAME="dashpay/machine"
+
 # Use regex pattern matching to check if "dev" exists in tag
-NPM_TAG="latest"
+DOCKER_TAG="latest"
 if [[ $PACKAGE_TAG =~ dev ]]; then
-  NPM_TAG="dev"
+  DOCKER_TAG="dev"
 fi
 
-# Now that checks have been passed, publish the module
-npm publish --tag $NPM_TAG
+docker build --build-arg NODE_ENV=development \
+             -t "${IMAGE_NAME}:${DOCKER_TAG}" \
+             -t "${IMAGE_NAME}:${VERSION}" \
+             .
+
+# Login to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Push images to the registry
+docker push "${IMAGE_NAME}:${DOCKER_TAG}"
+docker push "${IMAGE_NAME}:${VERSION}"
