168c17c9fe014fb97be589ea671126aa522a7211
Cofresi <cofresi@dash.org>
2019-11-26 20:19:59 -0400

    add tendermintRPCClient

---
+++ api/tendermintRPCClient.js
@@ -0,0 +1,35 @@
+const { client: JaysonClient } = require('jayson/promise');
+
+class tendermintRPCClient {
+  constructor() {
+    this.client = new JaysonClient({
+      host: process.env.TENDERMINT_HOST,
+      port: process.env.TENDERMINT_PORT,
+    });
+  }
+
+  /**
+   * Fetch transitions by tag
+   *
+   * @param {string} tag
+   * @param {boolean} prove
+   * @param {number} page
+   * @param {number} perPage
+   * @return {Promise<buffer[]|null>}
+   */
+  async fetchTaggedTransitions(tag, prove, page, perPage) {
+    const { result: transitions } = await this.client.request(
+      'tx_search',
+      {
+        query: tag,
+        prove,
+        page,
+        per_page: perPage,
+      },
+    );
+
+    return transitions;
+  }
+}
+
+module.exports = tendermintRPCClient;
