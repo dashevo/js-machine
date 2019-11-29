cb6883ee075cc316ce527f90af89c17273fe1e46
Cofresi <cofresi@dash.org>
2019-11-29 17:04:42 -0400

    add initial tendermint client unit test stubs

--- test/unit/api/TendermintRPCClient.spec.js
+++ test/unit/api/TendermintRPCClient.spec.js
@@ -1,22 +1,52 @@
+const TendermintRPCClient = require('../../../lib/api/TendermintRPCClient');
+const createTendermintRPCClientMock = require('../../../lib/test/mock/createTendermintRPCClientMock');
+
 describe('TendermintRPCClient', () => {
   let tendermintRpcMock;
-  const responseFixture = { totalCount: 12 };
 
-  beforeEach(() => {
-    tendermintRpcMock = {
-      fetchTransitionsByTag: this.sinon.stub(),
-    };
-
-    tendermintRpcMock
-      .fetchTransitionsByTag
-      .callsFake(async () => responseFixture);
+  beforeEach(function beforeEach() {
+    tendermintRpcMock = createTendermintRPCClientMock(this.sinon);
   });
 
-  it('should fetch a transition by tag', async () => {
+  it('should constuct a TendermintRPCClient instance', async () => {
+    const tendermintRPC = new TendermintRPCClient();
+
+    expect(tendermintRPC).to.be.instanceOf(TendermintRPCClient);
+    expect(tendermintRPC.client.options.host).to.be.a('string');
+    expect(tendermintRPC.client.options.port).to.be.a('string');
+  });
+
+  it('should fetch a transition by tag just with key', async () => {
+    const key = '';
+    const response = await tendermintRpcMock.fetchTransitionsByTag(key);
+
+    expect(response).to.be.instanceOf(Object);
+    expect(response).to.have.property('totalCount');
+    expect(response).to.deep.include({
+      totalCount: 12,
+    });
+  });
+
+  it('should fetch a transition by tag with key and value', async () => {
     const key = '';
     const value = '';
-    const response = await tendermintRpcMock.fetchTransitionsByTag(key, value, true);
+    const response = await tendermintRpcMock.fetchTransitionsByTag(key, value);
 
+    expect(response).to.be.instanceOf(Object);
+    expect(response).to.have.property('totalCount');
+    expect(response).to.deep.include({
+      totalCount: 12,
+    });
+  });
+
+  it('should fetch a transition by tag with key, value and prove', async () => {
+    const key = '';
+    const value = '';
+    const prove = true;
+    const response = await tendermintRpcMock.fetchTransitionsByTag(key, value, prove);
+
+    expect(response).to.be.instanceOf(Object);
+    expect(response).to.have.property('totalCount');
     expect(response).to.deep.include({
       totalCount: 12,
     });
