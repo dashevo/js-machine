28ae0a5f9e403e8ffce139ecf007a02e7ae8f9c4
Ivan Shumkov <ivan@shumkov.ru>
2019-12-01 15:59:36 +0300

    fix: wrong `dataProvider.fetchContract` implementation

--- test/unit/dpp/DataProvider.spec.js
+++ test/unit/dpp/DataProvider.spec.js
@@ -17,34 +17,72 @@ describe('DataProvider', () => {
     };
 
     driveApiClientMock = {
-      fetchContract: this.sinon.stub(),
+      request: this.sinon.stub(),
     };
 
     dataProvider = new DataProvider(driveApiClientMock, contractCacheMock);
   });
 
-  describe('#fetchContract', () => {
+  describe('#fetchDataContract', () => {
     it('should fetch contract from cache', async () => {
       contractCacheMock.get.returns(contract);
 
-      const actualContract = await dataProvider.fetchContract(contractId);
+      const actualContract = await dataProvider.fetchDataContract(contractId);
 
       expect(actualContract).to.equal(contract);
 
       expect(contractCacheMock.get).to.be.calledOnceWith(contractId);
-      expect(driveApiClientMock.fetchContract).to.not.be.called();
+      expect(driveApiClientMock.request).to.not.be.called();
     });
 
     it('should fetch contract from drive if it is not present in cache', async () => {
-      driveApiClientMock.fetchContract.resolves(contract);
+      driveApiClientMock.request.resolves({ result: contract });
 
-      const actualContract = await dataProvider.fetchContract(contractId);
+      const actualContract = await dataProvider.fetchDataContract(contractId);
 
-      expect(actualContract).to.equal(contract);
+      expect(actualContract.toJSON()).to.deep.equal({
+        $schema: 'https://schema.dash.org/dpp-0-4-0/meta/data-contract',
+        version: 1,
+        contractId: undefined,
+        documents: undefined,
+      });
 
-      expect(contractCacheMock.get).to.be.calledOnceWith(contractId);
-      expect(driveApiClientMock.fetchContract).to.be.calledOnceWith(contractId);
-      expect(contractCacheMock.set).to.be.calledOnceWith(contractId, contract);
+      expect(contractCacheMock.get).to.be.calledOnceWithExactly(contractId);
+      expect(driveApiClientMock.request).to.be.calledOnceWithExactly('fetchContract', { contractId });
+      expect(contractCacheMock.set).to.be.calledOnceWithExactly(contractId, actualContract);
+    });
+
+    it('should throw an error if received an error from Drive with invalid code', async () => {
+      const error = {
+        code: 42,
+        message: 'not the message you are looking for',
+      };
+
+      driveApiClientMock.request.resolves({
+        error,
+      });
+
+      try {
+        await dataProvider.fetchDataContract(contractId);
+
+        expect.fail('Error was not thrown');
+      } catch (e) {
+        expect(e.message).to.equal(`Can't fetch contract: ${error.message}`);
+      }
+    });
+
+    it('should return null if Drive returned invalid argument error', async () => {
+      const error = {
+        code: -32602, // invalid argument error
+      };
+
+      driveApiClientMock.request.resolves({
+        error,
+      });
+
+      const result = await dataProvider.fetchDataContract(contractId);
+
+      expect(result).to.be.null();
     });
   });
 });
