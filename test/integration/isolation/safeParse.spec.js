afe36693b10c330c13c92aab0864ac6b537bcc5d
Anton Suprunchuk <antouhou@gmail.com>
2020-01-28 20:02:32 +0800

    Make safeParse function work

--- test/integration/isolation/safeParse.spec.js
+++ test/integration/isolation/safeParse.spec.js
@@ -1,38 +1,27 @@
 const level = require('level-rocksdb');
-const cbor = require('cbor');
 
-const getIdentityFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityFixture');
 const getIdentityCreateSTFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityCreateSTFixture');
-const createDPPMock = require('@dashevo/dpp/lib/test/mocks/createDPPMock');
-const IdentityLevelDBRepository = require('../../../lib/identity/IdentityLevelDBRepository');
+
+const sinon = require('sinon');
+const createDppMock = require('@dashevo/dpp/lib/test/mocks/createDPPMock');
 const createSafeParse = require('../../../lib/isolation/safeParse');
 
-function initDppMock() {
-  const createDPPMock = require('@dashevo/dpp/lib/test/mocks/createDPPMock');
-  const getIdentityFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityFixture');
-}
+const identityCreateTransitionFixture = getIdentityCreateSTFixture();
+const dpp = createDppMock(sinon);
 
-describe('safeParse', () => {
+dpp.kek = 1;
+
+dpp
+  .stateTransition
+  .createFromSerialized
+  .resolves(identityCreateTransitionFixture);
+
+describe('safeParse', function () {
   let db;
-  let repository;
-  let identity;
-  let key;
-  let dppMock;
+  this.timeout(10000);
 
-  beforeEach(function beforeEach() {
+  beforeEach(() => {
     db = level('./db/identity-test', { valueEncoding: 'binary' });
-
-    identity = getIdentityFixture();
-
-    dppMock = createDPPMock(this.sinon);
-    dppMock
-      .identity
-      .createFromSerialized
-      .resolves(identity);
-
-    repository = new IdentityLevelDBRepository(db, dppMock);
-
-    key = `${IdentityLevelDBRepository.KEY_NAME}:${identity.getId()}`;
   });
 
   afterEach(async () => {
@@ -40,12 +29,12 @@ describe('safeParse', () => {
     await db.close();
   });
 
-  it('should store identity', async () => {
-    const safeParse = createSafeParse(`${initDppMock}`);
-    const identityCreateST = getIdentityCreateSTFixture();
+  it('should parse state transition', async () => {
+    const safeParse = createSafeParse(dpp);
+    const serializedIdentityCreateST = getIdentityCreateSTFixture().serialize();
 
-    const parsedTransition = safeParse(identityCreateST);
+    const parsedTransition = await safeParse(serializedIdentityCreateST);
 
-    throw new Error('Not implemented');
+    expect(parsedTransition).to.be.deep.equal(getIdentityCreateSTFixture());
   });
 });
