33887708a651501dab124541f2173cb64453dc3a
Anton Suprunchuk <antouhou@gmail.com>
2020-02-05 13:59:45 +0800

    Add browserify to load dpp module

--- test/integration/isolation/safeParse.spec.js
+++ test/integration/isolation/safeParse.spec.js
@@ -1,40 +1,51 @@
-const level = require('level-rocksdb');
+const sinon = require('sinon');
+const DashPlatformProtocol = require('@dashevo/dpp');
 
 const getIdentityCreateSTFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityCreateSTFixture');
 
-const sinon = require('sinon');
-const createDppMock = require('@dashevo/dpp/lib/test/mocks/createDPPMock');
 const createSafeParse = require('../../../lib/isolation/safeParse');
-
-const identityCreateTransitionFixture = getIdentityCreateSTFixture();
-const dpp = createDppMock(sinon);
-
-dpp.kek = 1;
-
-dpp
-  .stateTransition
-  .createFromSerialized
-  .resolves(identityCreateTransitionFixture);
+const createDataProviderMock = require('@dashevo/dpp/lib/test/mocks/createDataProviderMock');
 
 describe('safeParse', function () {
-  let db;
-  this.timeout(10000);
+  let identityCreateTransitionFixture;
+  let dataProvideMock;
+  this.timeout(100000);
 
   beforeEach(() => {
-    db = level('./db/identity-test', { valueEncoding: 'binary' });
-  });
-
-  afterEach(async () => {
-    await db.clear();
-    await db.close();
+    dataProvideMock = createDataProviderMock(sinon);
+    identityCreateTransitionFixture = getIdentityCreateSTFixture();
   });
 
   it('should parse state transition', async () => {
-    const safeParse = createSafeParse(dpp);
+    console.time('create');
+    const safeParse = await createSafeParse(dataProvideMock);
+    console.timeEnd('create');
     const serializedIdentityCreateST = getIdentityCreateSTFixture().serialize();
 
-    const parsedTransition = await safeParse(serializedIdentityCreateST);
+    // const dpp = new DashPlatformProtocol();
+    // const st = await dpp.stateTransition.createFromSerialized(serializedIdentityCreateST, { skipValidation: true });
+    // console.log(st);
+    console.time('parse');
+    const parsedTransition = await safeParse(serializedIdentityCreateST, getIdentityCreateSTFixture().toJSON());
+    console.timeEnd('parse');
 
     expect(parsedTransition).to.be.deep.equal(getIdentityCreateSTFixture());
+  });
+
+  it('Should stop execution if dpp validation takes too much memory', async () => {
+    console.time('create');
+    const safeParse = await createSafeParse(dataProvideMock);
+    console.timeEnd('create');
+    const serializedIdentityCreateST = getIdentityCreateSTFixture().serialize();
+
+    console.time('parse');
+    const parsedTransition = await safeParse(serializedIdentityCreateST);
+    console.timeEnd('parse');
+
+    expect(parsedTransition).to.be.deep.equal(getIdentityCreateSTFixture());
+  });
+
+  it('Should stop execution if schema takes too much time', async () => {
+    throw new Error('Not implemented');
   });
 });
