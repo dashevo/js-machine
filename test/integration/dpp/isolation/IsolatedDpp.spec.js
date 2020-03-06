13c5786c812e10ba83192768c0ce78d02b0ff944
Djavid Gabibyian <djavidoff@gmail.com>
2020-03-06 22:21:29 +0500

    chore: initial integration tests for `IsolatedDpp`

--- test/integration/dpp/isolation/IsolatedDpp.spec.js
+++ test/integration/dpp/isolation/IsolatedDpp.spec.js
@@ -1,165 +1,271 @@
-const sinon = require('sinon');
-const { Reference } = require('isolated-vm');
-const { Transaction } = require('@dashevo/dashcore-lib');
-const Document = require('@dashevo/dpp/lib/document/Document');
+const { Isolate } = require('isolated-vm');
+
 const DashPlatformProtocol = require('@dashevo/dpp');
-const IdentityPublicKey = require('@dashevo/dpp/lib/identity/IdentityPublicKey');
-const ValidationResult = require('@dashevo/dpp/lib/validation/ValidationResult');
-const { PrivateKey } = require('@dashevo/dashcore-lib');
+const createDataProviderMock = require('@dashevo/dpp/lib/test/mocks/createDataProviderMock');
+const getDocumentsFixture = require('@dashevo/dpp/lib/test/fixtures/getDocumentsFixture');
 const getDataContractFixture = require('@dashevo/dpp/lib/test/fixtures/getDataContractFixture');
 const getIdentityFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityFixture');
-const createDppMock = require('@dashevo/dpp/lib/test/mocks/createDPPMock');
-const getIdentityStateTransitionFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityCreateSTFixture');
-const createDataProviderMock = require('@dashevo/dpp/lib/test/mocks/createDataProviderMock');
-const createIsolatedDpp = require('../../../../lib/dpp/isolation/createIsolatedDpp');
+const getIdentityCreateSTFixture = require(
+  '@dashevo/dpp/lib/test/fixtures/getIdentityCreateSTFixture',
+);
 
-const IsolatedDataProviderWrapper = require('../../../../lib/dpp/isolation/IsolatedDataProviderWrapper');
+const InvalidStateTransitionError = require('@dashevo/dpp/lib/stateTransition/errors/InvalidStateTransitionError');
+const InvalidDataContractError = require('@dashevo/dpp/lib/dataContract/errors/InvalidDataContractError');
+const InvalidDocumentError = require('@dashevo/dpp/lib/document/errors/InvalidDocumentError');
+const InvalidIdentityError = require('@dashevo/dpp/lib/identity/errors/InvalidIdentityError');
 
-describe('IsolatedDpp', function () {
-  let dataProviderMock;
-  let dataProviderMockReference;
-  let dataProviderWrapper;
-  let dataContractFixture;
-  let identitySTFixture;
-  let transactionFixture;
-  let dppMock;
+const JsonSchemaError = require('@dashevo/dpp/lib/errors/JsonSchemaError');
+
+const IsolatedDpp = require('../../../../lib/dpp/isolation/IsolatedDpp');
+const compileFileWithBrowserify = require(
+  '../../../../lib/dpp/isolation/compileFileWithBrowserify',
+);
+
+describe('IsolatedDpp', function main() {
+  this.timeout(10000);
+
+  let isolateSnapshot;
   let isolatedDpp;
-  let privateKey;
+  let isolateOptions;
+  let dataProviderMock;
   let dpp;
-  this.timeout(100000);
 
-  beforeEach(async () => {
-    dataProviderMock = createDataProviderMock(sinon);
-    dataProviderMockReference = new Reference(dataProviderMock);
-    dataProviderWrapper = new IsolatedDataProviderWrapper(dataProviderMockReference);
-    dataContractFixture = getDataContractFixture();
-    identitySTFixture = getIdentityStateTransitionFixture();
-    privateKey = new PrivateKey();
-    identitySTFixture.publicKeys = [new IdentityPublicKey({
-      id: 1,
-      type: IdentityPublicKey.TYPES.ECDSA_SECP256K1,
-      data: privateKey.toPublicKey().toBuffer().toString('base64'),
-      isEnabled: true,
-    })];
-    identitySTFixture.sign(identitySTFixture.getPublicKeys()[0], privateKey);
-    dppMock = createDppMock(sinon);
-    isolatedDpp = await createIsolatedDpp(dataProviderMock, 128, 5000);
-    dpp = new DashPlatformProtocol({ dataProvider: dataProviderMock });
-    transactionFixture = new Transaction('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858eeac00000000');
+  let dataContract;
+  let document;
+  let stateTransition;
+  let identity;
+
+  before(async () => {
+    const isolateDataProviderCode = await compileFileWithBrowserify(
+      './internal/createDataProviderWrapper', 'createDataProvider',
+    );
+    const isolateDppCode = await compileFileWithBrowserify(
+      './internal/createDpp', 'createDpp',
+    );
+    const isolateTimeoutShimCode = await compileFileWithBrowserify(
+      './internal/createTimeoutShim', 'createTimeoutShim',
+    );
+
+    isolateSnapshot = await Isolate.createSnapshot([
+      { code: isolateDataProviderCode },
+      { code: isolateDppCode },
+      { code: isolateTimeoutShimCode },
+    ]);
   });
 
-  describe('stateTransition', () => {
-    describe('createFromSerialized', () => {
+  beforeEach(function beforeEach() {
+    isolateOptions = {
+      isolateOptions: {
+        memoryLimit: 128,
+      },
+      executionOptions: {
+        arguments: {
+          copy: true,
+        },
+        result: {
+          promise: true,
+        },
+        timeout: 5000,
+      },
+    };
+
+    [document] = getDocumentsFixture();
+    dataContract = getDataContractFixture();
+    identity = getIdentityFixture();
+    stateTransition = getIdentityCreateSTFixture();
+
+    dataProviderMock = createDataProviderMock(this.sinon);
+
+    dpp = new DashPlatformProtocol({ dataProvider: dataProviderMock });
+
+    isolatedDpp = new IsolatedDpp(dpp, isolateSnapshot, isolateOptions);
+  });
+
+  describe('dataContract', () => {
+    describe('#create', () => {
       it('should act the same way as not isolated dpp does', async () => {
-        const serialized = identitySTFixture.serialize();
-        let actualSt = await isolatedDpp.stateTransition.createFromSerialized(serialized);
-
-        expect(serialized).to.be.instanceOf(Buffer);
-        expect(actualSt).to.be.deep.equal(identitySTFixture);
-
-        actualSt = await isolatedDpp.stateTransition.createFromSerialized(serialized.toString('hex'));
-
-        expect(actualSt).to.be.deep.equal(identitySTFixture);
+        throw new Error('Not implemented');
       });
     });
-    describe('createFromObject', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        const json = identitySTFixture.toJSON();
-        const actualSt = await isolatedDpp.stateTransition.createFromObject(json);
 
-        expect(actualSt).to.be.deep.equal(identitySTFixture);
+    describe('#createFromObject', () => {
+      it('should act the same way as not isolated dpp does', async () => {
+        throw new Error('Not implemented');
       });
     });
-    describe('validate', () => {
-      it('should act the same way as not isolated dpp does when it is valid', async () => {
-        const expectedValidationResult = await dpp.stateTransition.validate(identitySTFixture);
-        const actualValidationResult = await isolatedDpp.stateTransition.validate(
-          identitySTFixture,
-        );
 
-        expect(actualValidationResult.isValid()).to.be.true();
-        expect(actualValidationResult).to.be.deep.equal(expectedValidationResult);
+    describe('#validate', () => {
+      it('should act the same way as not isolated dpp does', async () => {
+        throw new Error('Not implemented');
       });
-      it('should act the same way as not isolated dpp does when it is not valid', async () => {
-        const expectedValidationResult = await dpp.stateTransition.validate(
-          getIdentityStateTransitionFixture(),
-        );
-        const actualValidationResult = await isolatedDpp.stateTransition.validate(
-          getIdentityStateTransitionFixture(),
-        );
+    });
 
-        expect(expectedValidationResult.isValid()).to.be.false();
-        expect(actualValidationResult).to.be.deep.equal(expectedValidationResult);
+    describe('#createFromSerialized', () => {
+      it('should pass through validation result', async () => {
+        delete dataContract.ownerId;
+        try {
+          await isolatedDpp.dataContract.createFromSerialized(
+            dataContract.serialize(),
+          );
+          expect.fail('Error was not thrown');
+        } catch (e) {
+          expect(e).to.be.an.instanceOf(InvalidDataContractError);
+
+          const [error] = e.getErrors();
+          expect(error).to.be.an.instanceOf(JsonSchemaError);
+        }
+      });
+
+      it('should create data contract from serialized data', async () => {
+        const result = await isolatedDpp.dataContract.createFromSerialized(
+          dataContract.serialize(),
+        );
+        expect(result.toJSON()).to.deep.equal(dataContract.toJSON());
       });
     });
   });
 
   describe('document', () => {
-    describe('create', () => {
+    describe('#create', () => {
       it('should act the same way as not isolated dpp does', async () => {
         throw new Error('Not implemented');
       });
     });
-    describe('createFromSerialized', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        throw new Error('Not implemented');
-      });
-    });
-    describe('createFromObject', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        throw new Error('Not implemented');
-      });
-    });
-    describe('validate', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        throw new Error('Not implemented');
-      });
-    });
-  });
 
-  describe('dataContract', () => {
-    describe('create', () => {
+    describe('#createFromObject', () => {
       it('should act the same way as not isolated dpp does', async () => {
         throw new Error('Not implemented');
       });
     });
-    describe('createFromSerialized', () => {
+
+    describe('#validate', () => {
       it('should act the same way as not isolated dpp does', async () => {
         throw new Error('Not implemented');
       });
     });
-    describe('createFromObject', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        throw new Error('Not implemented');
+
+    describe('#createFromSerialized', () => {
+      it('should pass through validation result', async () => {
+        delete document.id;
+
+        try {
+          await isolatedDpp.document.createFromSerialized(
+            document.serialize(),
+          );
+          expect.fail('Error was not thrown');
+        } catch (e) {
+          expect(e).to.be.an.instanceOf(InvalidDocumentError);
+
+          const [error] = e.getErrors();
+          expect(error).to.be.an.instanceOf(JsonSchemaError);
+        }
       });
-    });
-    describe('validate', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        throw new Error('Not implemented');
+
+      it('should create document from serialized data', async () => {
+        const result = await isolatedDpp.document.createFromSerialized(
+          document.serialize(),
+        );
+        expect(result.toJSON()).to.deep.equal(document.toJSON());
       });
     });
   });
 
   describe('identity', () => {
-    describe('create', () => {
+    describe('#create', () => {
       it('should act the same way as not isolated dpp does', async () => {
         throw new Error('Not implemented');
       });
     });
-    describe('createFromSerialized', () => {
+
+    describe('#createFromObject', () => {
       it('should act the same way as not isolated dpp does', async () => {
         throw new Error('Not implemented');
       });
     });
+
+    describe('#validate', () => {
+      it('should act the same way as not isolated dpp does', async () => {
+        throw new Error('Not implemented');
+      });
+    });
+
+    describe('#createFromSerialized', () => {
+      it('should pass through validation result', async () => {
+        delete identity.id;
+        try {
+          await isolatedDpp.identity.createFromSerialized(
+            identity.serialize(),
+          );
+          expect.fail('Error was not thrown');
+        } catch (e) {
+          expect(e).to.be.an.instanceOf(InvalidIdentityError);
+
+          const [error] = e.getErrors();
+          expect(error).to.be.an.instanceOf(JsonSchemaError);
+        }
+      });
+
+      it('should create identity from serialized data', async () => {
+        const result = await isolatedDpp.identity.createFromSerialized(
+          identity.serialize(),
+        );
+        expect(result.toJSON()).to.deep.equal(identity.toJSON());
+      });
+    });
+  });
+
+  describe('stateTransition', () => {
+    describe('#createFromSerialized', () => {
+      it('should pass through validation result', async () => {
+        delete stateTransition.lockedOutPoint;
+
+        try {
+          await isolatedDpp.stateTransition.createFromSerialized(
+            stateTransition.serialize(),
+          );
+          expect.fail('Error was not thrown');
+        } catch (e) {
+          expect(e).to.be.an.instanceOf(InvalidStateTransitionError);
+
+          const [error] = e.getErrors();
+          expect(error).to.be.an.instanceOf(JsonSchemaError);
+        }
+      });
+
+      it('should create state transition from serialized data', async () => {
+        const result = await isolatedDpp.stateTransition.createFromSerialized(
+          stateTransition.serialize(),
+        );
+        expect(result.toJSON()).to.deep.equal(dataContract.toJSON());
+      });
+    });
+
     describe('createFromObject', () => {
       it('should act the same way as not isolated dpp does', async () => {
+        const result = await isolatedDpp.stateTransition.createFromObject(
+          stateTransition.toJSON(),
+        );
+
+        expect(result).to.be.deep.equal(stateTransition.toJSON());
+      });
+    });
+
+    describe('#validate', () => {
+      it('should act the same way as not isolated dpp does when it is valid', async () => {
+        throw new Error('Not implemented');
+      });
+
+      it('should act the same way as not isolated dpp does when it is not valid', async () => {
         throw new Error('Not implemented');
       });
     });
-    describe('validate', () => {
-      it('should act the same way as not isolated dpp does', async () => {
-        throw new Error('Not implemented');
-      });
-    });
+  });
+
+  it('should stop execution if dpp validation takes too much memory', async () => {
+    throw new Error('Not implemented');
+  });
+
+  it('should stop execution if dpp validation takes too much time', async () => {
+    throw new Error('Not implemented');
   });
 });
