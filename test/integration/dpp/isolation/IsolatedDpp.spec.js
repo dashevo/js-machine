ea0f5d87f02ff6414c4ed441bdd90545b69d4f9c
Anton Suprunchuk <antouhou@gmail.com>
2020-03-10 13:38:03 +0700

    fix: IsolatedDpp throws error if location is not defined
    Co-authored-by: Anton Suprunchuk <antouhou@gmail.com>
    Co-authored-by: Ivan Shumkov <ivan@shumkov.ru>

--- test/integration/dpp/isolation/IsolatedDpp.spec.js
+++ test/integration/dpp/isolation/IsolatedDpp.spec.js
@@ -112,7 +112,7 @@ describe('IsolatedDpp', function main() {
 
     describe('#createFromSerialized', () => {
       it('should pass through validation result', async () => {
-        delete dataContract.ownerId;
+        delete dataContract.contractId;
 
         try {
           await isolatedDpp.dataContract.createFromSerialized(
